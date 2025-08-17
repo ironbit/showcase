fd363583cea9c054639848077113eaa63545df89
Christian Noboa Mardini <ironbit@users.noreply.github.com>
2025-08-17 00:24:54 -0500

    vm | add 'vm' module in lvm
    * add 'context' component in module.

--- virtual-machine/lvm/src/lib.rs
+++ virtual-machine/lvm/src/lib.rs
@@ -2,3 +2,4 @@
 
 pub mod core;
 pub mod mem;
+pub mod vm;
---
+++ virtual-machine/lvm/src/vm/context.rs
@@ -0,0 +1,42 @@
+use {
+    crate::{core, mem},
+    std::fmt,
+};
+
+#[derive(Debug)]
+pub struct Context<S>
+where
+    S: mem::StackExt + fmt::Debug,
+{
+    stack: S,
+    counter: core::Index,
+}
+
+impl<S> Context<S>
+where
+    S: mem::StackExt + fmt::Debug,
+{
+    pub fn new(stack: S) -> Self {
+        let counter = core::Index::default();
+
+        Self { stack, counter }
+    }
+
+    pub fn reset(&mut self) {
+        self.stack.reset();
+    }
+
+    pub fn stack(&mut self) -> &mut S {
+        &mut self.stack
+    }
+
+    pub fn load_counter(&mut self) -> core::Index {
+        let counter = self.counter;
+        self.counter += 1;
+        counter
+    }
+
+    pub fn save_counter(&mut self, index: core::Index) {
+        self.counter = index;
+    }
+}
